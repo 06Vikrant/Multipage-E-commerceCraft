131e3c491b844ed74d413cdeca489c0b4c086471
Vikrant <06learntocode11@gmial.com>
2024-05-14 16:05:53 +0530

    sign-in section ✅

---
+++ src/components/auth/AuthOptions.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+import { defaultTheme } from "../../styles/themes/default";
+import { staticImages } from "../../utils/imgaes";
+
+const SignOptions = styled.div`
+  row-gap: 12px;
+
+  .sign-option {
+    column-gap: 12px;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid ${defaultTheme.color_platinum};
+    transition: ${defaultTheme.default_transition};
+
+    &:hover {
+      transform: translateY(2px);
+      border-color: ${defaultTheme.color_white};
+    }
+
+    .sign-opt-icon {
+      img {
+        width: 18px;
+      }
+    }
+  }
+`;
+
+const AuthOptions = () => {
+  return (
+    <SignOptions className="grid">
+      <Link to="/" className="sign-option flex items-center justify-center">
+        <span className="sign-opt-icon flex items-center justify-center">
+          <img src={staticImages.google} alt="" />
+        </span>
+        <span className="sign-opt-text font-medium">Continue With Google</span>
+      </Link>
+      <Link to="/" className="sign-option flex items-center justify-center">
+        <span className="sign-opt-icon flex items-center justify-center">
+          <img src={staticImages.twitter} alt="" />
+        </span>
+        <span className="sign-opt-text font-medium">Continue With Twitter</span>
+      </Link>
+    </SignOptions>
+  );
+};
+
+export default AuthOptions;
---
+++ src/pages/auth/SignIn.jsx
@@ -0,0 +1,105 @@
+import { styled } from "styled-components";
+import AuthOptions from "../../components/auth/AuthOptions";
+import { FormGridWrapper, FormTitle } from "../../styles/themes/form_grid";
+import { Container } from "../../styles/themes/styles";
+import { staticImages } from "../../utils/imgaes";
+import { FormElement, Input } from "../../styles/themes/form";
+import { PasswordInput } from "../../components";
+import { Link } from "react-router-dom";
+import { BaseButtonBlack, BaseButtonGreen } from "../../styles/themes/button";
+import { defaultTheme } from "../../styles/themes/default";
+
+const SignInScreenWrapper = styled.section`
+  .form-seperator {
+    margin: 32px 0;
+    column-gap: 18px;
+
+    @media (max-width: 768px) {
+      margin: 24px 0;
+    }
+
+    .seperator-text {
+      border-radius: 50%;
+      min-width: 36px;
+      height: 36px;
+      background-color: ${defaultTheme.color_purple};
+      position: relative;
+    }
+
+    .seperator-line {
+      width: 100%;
+      height: 1px;
+      background-color: ${defaultTheme.color_platinum};
+    }
+  }
+
+  .form-elem-text {
+    margin-top: -16px;
+    display: block;
+  }
+`;
+
+const SignIn = () => {
+  return (
+    <SignInScreenWrapper>
+      <FormGridWrapper>
+        <Container>
+          <div className="form-grid-content">
+            <div className="form-grid-left">
+              <img src={staticImages.form_img1} className="object-fit-cover" />
+            </div>
+            <div className="form-grid-right">
+              <FormTitle>
+                <h3>Sign In</h3>
+              </FormTitle>
+              <AuthOptions />
+
+                <div className="form-seperator flex items-center justify-center">
+                  <span className="seperator-line"></span>
+                  <span className="seperator-text inline-flex items-center justify-center text-white">
+                    OR
+                  </span>
+                  <span className="seperator-line"></span>
+                </div>
+
+                <form action="">
+                  <FormElement>
+                    <label htmlFor="" className="form-elem-label">
+                      Username or email address
+                    </label>
+                    <Input
+                      type="text"
+                      placeholder=""
+                      name=""
+                      className="form-elem-control"
+                    />
+                  </FormElement>
+                  <PasswordInput fieldName="Password" name="password" />
+                  <Link
+                    to="/reset"
+                    className="form-elem-text text-end font-medium"
+                  >
+                    Forgot your password?
+                  </Link>
+                  <BaseButtonBlack 
+                    type='submit'
+                    className='form-submit-btn'
+                  >
+                    Sign In
+                  </BaseButtonBlack>
+                </form>
+                <p className="flex flex-wrap account-rel-text">
+                  Don&apos;t have a account?
+                  <Link to='/sing_up'>
+                      Sign Up  
+                  </Link>
+                </p>
+            </div>
+          </div>
+        </Container>
+      </FormGridWrapper>
+    </SignInScreenWrapper>
+  );
+};
+
+export default SignIn;
