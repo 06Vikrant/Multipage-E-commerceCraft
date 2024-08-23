1f33e958b6cc9ea85499faef52a1a7400175f41c
Vikrant <06learntocode11@gmial.com>
2024-08-23 13:21:33 +0530

    Home screen ✅

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,23 @@
+import { styled } from "styled-components"
+import { Brands, Catalog, Featured, Feedback, Hero, NewArrival, SavingZone } from "../../components";
+import { limelightCatalog, mensCatalog, womensCatalog } from "../../data/data";
+const HomeScreenWrapper = styled.main`
+
+`;
+
 const Home = () => {
   return (
-    <div>
-      Home page
-    </div>
+    <HomeScreenWrapper>
+      <Hero />
+      <Featured />
+      <NewArrival />
+      <SavingZone />
+      <Catalog catalogTitle={'Catogries For Men'} products={mensCatalog}/>
+      <Catalog catalogTitle={'Catogries For Women'} products={womensCatalog}/>
+      <Brands />
+      <Catalog catalogTitle={'In The Lime Light'} products={limelightCatalog}/>
+      <Feedback />
+    </HomeScreenWrapper>
   )
 }
 
